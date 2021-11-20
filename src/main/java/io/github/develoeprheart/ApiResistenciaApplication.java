e2ee16051fba5de00718da45450c1b38a328cd46
Developerheart <leonardonascimento897@gmail.com>
2021-11-20 20:21:07 -0300

    base do projeto criada

---
+++ src/main/java/io/github/develoeprheart/ApiResistenciaApplication.java
@@ -0,0 +1,13 @@
+package io.github.develoeprheart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResistenciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiResistenciaApplication.class, args);
+    }
+
+}
